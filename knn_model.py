f764aa2c667b19c57ecdc48b2a1cc86e59fa8cad
AravindMandan <aravind.mandan@minfytech.com>
2025-07-08 15:25:41 +0530

    Update knn_model.py 2

--- knn_model.py
+++ knn_model.py
@@ -8,7 +8,7 @@ from sklearn.metrics import accuracy_score
 # Load dataset
 data = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
-    data.data, data.target, test_size=0.2, random_state=42
+    data.data, data.target, test_size=0.3, random_state=42
 )
  
 # Train KNN model
